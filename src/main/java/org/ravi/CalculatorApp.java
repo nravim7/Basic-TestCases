2ec62eda9ccc6078f611def573fc841dad90ce78
Ravi N <your.email@example.com>
2024-01-11 08:41:15 +0530

    yfyddydy

---
+++ src/main/java/org/ravi/CalculatorApp.java
@@ -0,0 +1,8 @@
+package org.ravi;
+
+public class CalculatorApp {
+    public int divisionMethod(int a, int b) {
+
+        return a/b;
+    }
+}
---
+++ src/test/java/org/ravi/CalculatorAppTest.java
@@ -0,0 +1,11 @@
+package org.ravi;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorAppTest {
+
+    @org.junit.jupiter.api.Test
+    void divisionMethod() {
+        fail("nothing provided");
+    }
+}
