7c289bcb9f7fc25c63a2e4d56bd3de7c7b641450
Ravi N <your.email@example.com>
2024-01-11 19:58:20 +0530

    @DisplayName Annotation

--- src/test/java/org/ravi/CalculatorAppTest.java
+++ src/test/java/org/ravi/CalculatorAppTest.java
@@ -1,17 +1,21 @@
 package org.ravi;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Tests Calculator App")
 class CalculatorAppTest {
     CalculatorApp calculatorApp = new CalculatorApp();
 
+    @DisplayName("Test 8/2 = 4")
     @Test
-    void integerDivision() {
+    void testIntegerDivision_WhenEightIsDividedByTwo_ShouldReturnFour() {
         assertEquals(4, calculatorApp.integerDivision(8,2), "divisionMethod Failed");
     }
 
+    @DisplayName("Test 4-2 =2")
     @Test
     void integerSubtraction() {
         assertEquals(2, calculatorApp.integerSubtraction(4,2), "Subtraction Method failed");
